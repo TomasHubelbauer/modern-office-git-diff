140739480b494622a518f0fd22a46fd84ed05543
Tomáš Hübelbauer <tomas@hubelbauer.net>
2017-12-31 09:42:44 +0100

    Track changed generated files instead of waiting

--- cmd/pre-commit.ps1
+++ cmd/pre-commit.ps1
@@ -6,7 +6,5 @@ Get-ChildItem out/Title -Filter *.xml -Recurse |
 Foreach-Object {
 	$xml = [xml](Get-Content -literalPath $_.FullName)
 	$xml.Save($_.FullName)
+	git add "$_.FullName"
 }
-
-# Wait for a bit so we don't have dangling files
-Start-Sleep -Seconds 5

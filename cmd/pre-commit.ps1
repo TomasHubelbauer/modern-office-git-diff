68e98d0f1417b39dfe1cccb5baeac16bfe98247d
Tomáš Hübelbauer <tomas@hubelbauer.net>
2017-12-31 09:23:02 +0100

    Add dangling files and try to format XML

--- cmd/pre-commit.ps1
+++ cmd/pre-commit.ps1
@@ -1,3 +1,9 @@
 Copy-Item Title.docx Title.zip
 Expand-Archive Title.zip -DestinationPath out/Title -Force
 Remove-Item Title.zip
+# Format XML files
+Get-ChildItem out/Title -Filter *.xml -Recurse |
+Foreach-Object {
+	$xml = [xml](Get-Content $_.FullName)
+	$xml.Save($_.FullName)
+}
